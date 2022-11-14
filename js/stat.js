ba8a991320511b8933765178fffd5c35bb4b7e8d
Андрей <Andreyadad@gmail.com>
2022-11-14 15:38:34 +0500

    Добавил еще игроков и выровнял гистограмму

--- js/stat.js
+++ js/stat.js
@@ -11,4 +11,15 @@ var renderCloud = function (ctx, x, y, color) {
 window.renderStatistics = function (ctx) {
 	renderCloud(ctx, 110, 60, 'rgba(0, 0, 0, 0.3)');
 	renderCloud(ctx, 100, 50, '#fff');
+
+	ctx.fillStyle = '#000';
+	ctx.fillText('Вы', 110, 75);
+	ctx.fillRect(160, 60, 430, 20);
+
+	ctx.fillText('Иван', 110, 105);
+	ctx.fillRect(160, 90, 430, 20);
+	
+	ctx.fillText('Юлия', 110, 135);
+	ctx.fillRect(160, 120, 430, 20);
+
 };
