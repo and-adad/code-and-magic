5d0e0039868c98b4bebb30c8614a1d43763e4663
Андрей <Andreyadad@gmail.com>
2022-11-14 13:48:14 +0500

    Перенес в функцию отрисовку облачка на канвасе

--- js/stat.js
+++ js/stat.js
@@ -1,9 +1,14 @@
 'use strict';
 
-window.renderStatistics = function (ctx) {
-	ctx.fillStyle = '#fff';
-	ctx.fillRect(100, 50, 500, 200);
+var renderCloud = function (ctx, x, y, color) {
+	ctx.fillStyle = color;
+	ctx.fillRect(x, y, 500, 200);
+};
 
+window.renderStatistics = function (ctx) {
 	ctx.fillStyle = 'rgba(0, 0, 0, 0.3';
 	ctx.fillRect(110, 60, 500, 200);
+
+	ctx.fillStyle = '#fff';
+	ctx.fillRect(100, 50, 500, 200);
 };
