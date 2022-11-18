7e0d7a329cba45d96674220e28a9f334001b59c1
Андрей <Andreyadad@gmail.com>
2022-11-18 13:43:35 +0500

    Под обдаком добавил тень: многоугольник такой же формы, залитый цветом rgba(0, 0, 0, 0.7)(полупрозрачный черный), смещенный относительно белого на 10px вниз и вправо

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,20 @@
 'use strict';
 
 window.renderStatistics = function (ctx) {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.beginPath();
+    ctx.moveTo(110, 20);
+    ctx.lineTo(320, 10);
+    ctx.lineTo(530, 20);
+    ctx.lineTo(540, 155);
+    ctx.lineTo(530, 290);
+    ctx.lineTo(320, 300);
+    ctx.lineTo(110,290);
+    ctx.lineTo(100, 155);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+
     ctx.fillStyle = '#fff';
     ctx.beginPath();
     ctx.moveTo(100, 10);
