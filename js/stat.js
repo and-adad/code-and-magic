8a80ee6f93fda2ad78812a956ac600b00c7a4aa0
Андрей <Andreyadad@gmail.com>
2022-11-18 16:52:29 +0500

    Уставовил алгоритм отрисовки столбиков гистограммы. При помощи цикла из массива отрисовал для четырех участников гистограмму.

--- js/stat.js
+++ js/stat.js
@@ -2,9 +2,13 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
-var CLOUD_DELTA = 10; // это кратчайшее расстояние (как по оси x, так и по оси y) от грани многоугольника до ребра прямоугольника вписанного в наш многоугольник
+var BAR_DELTA = 35; // это расстояние между столбиками гистограммы
+var CLOUD_DELTA = 10; // это кратчайшее расстояние (как по оси x, так и по оси y) от грани многоугольника до ребра прямоугольника вписанного в наш многоугольник. А также расстояние, на которое тень отстает от облака.
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
+var TEXT_HEIGHT = 16;
+var BAR_WIDTH = 40;
+var BAR_HEIGHT = 150;
 
 var renderCloud = function(ctx, CLOUD_X, CLOUD_Y, color) {
     ctx.fillStyle = color;
@@ -24,11 +28,28 @@ var renderCloud = function(ctx, CLOUD_X, CLOUD_Y, color) {
 
 window.renderStatistics = function (ctx) {
 
-    renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
+    renderCloud(ctx, CLOUD_X + CLOUD_DELTA, CLOUD_Y + CLOUD_DELTA, 'rgba(0, 0, 0, 0.7)');
     renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
     ctx.fillStyle = '#000';
-    ctx.font = '16px Mono';
-    ctx.fillText('Ура вы победили!', 255, 20);
-    ctx.strokeText('Список результатов:', 245, 40);
+    ctx.font = '16px PT Mono';
+    ctx.fillText('Ура вы победили!', 255, 35);
+    ctx.strokeText('Список результатов:', 245, 80);
+
+ /*   var playerIndex = 0;
+    var playerName = 'Вы';*/
+
+    var players = ['Вы', 'Валя', 'Кекс', 'Катя'];
+
+    for (var i = 0; i < players.length; i++) {
+        console.log(i);
+        ctx.fillText(players[i], CLOUD_X + BAR_DELTA + (BAR_WIDTH + BAR_DELTA)*i, CLOUD_Y + CLOUD_HEIGHT -CLOUD_DELTA);
+        ctx.fillRect(CLOUD_X + BAR_DELTA + (BAR_WIDTH + BAR_DELTA)*i, CLOUD_Y + CLOUD_HEIGHT -CLOUD_DELTA - TEXT_HEIGHT, BAR_WIDTH, - BAR_HEIGHT);
+    }
+
+    /*ctx.fillText('playerName', CLOUD_X + CLOUD_DELTA, CLOUD_Y + CLOUD_HEIGHT -CLOUD_DELTA);
+    ctx.fillRect(CLOUD_X + CLOUD_DELTA, CLOUD_Y + CLOUD_HEIGHT -CLOUD_DELTA - TEXT_HEIGHT, BAR_WIDTH, - BAR_HEIGHT);
+
+    ctx.fillText('Валя', CLOUD_X + CLOUD_DELTA + BAR_WIDTH + CLOUD_DELTA, CLOUD_Y + CLOUD_HEIGHT -CLOUD_DELTA);
+    ctx.fillRect(CLOUD_X + CLOUD_DELTA + BAR_WIDTH + CLOUD_DELTA, CLOUD_Y + CLOUD_HEIGHT -CLOUD_DELTA - TEXT_HEIGHT, BAR_WIDTH, - BAR_HEIGHT);*/
 };
