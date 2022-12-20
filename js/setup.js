b32e06520a6e6cdad73cd2980dbddeb906cb5455
Андрей <Andreyadad@gmail.com>
2022-12-20 19:55:35 +0500

    Покажем блок с похожими персонажами. Найдем шаблон, который мы будем копировать. И найдем элемент, в который мы будем вставлять похожих магов

---
+++ js/setup.js
@@ -0,0 +1,13 @@
+// Файл setup.js
+
+'use strict';
+
+var userDialog = document.querySelector('.setup');
+
+userDialog.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
