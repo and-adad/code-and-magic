9524a5504ae63918668a4082fac55fc8fb26816c
Андрей <Andreyadad@gmail.com>
2022-12-20 20:40:36 +0500

    Добавим магам цвета плащей

--- js/setup.js
+++ js/setup.js
@@ -1,25 +1,42 @@
 // Файл setup.js
-
 'use strict';
 
 var userDialog = document.querySelector('.setup');
-
 userDialog.classList.remove('hidden');
 
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 var similarListElement = document.querySelector('.setup-similar-list');
-
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 var WIZARD_NAMES = ['Дамблдор', 'Волдемарт', 'Доктор Стрэндж', 'Гарри Поттер'];
 
-for (var i = 0; i < 4; i++) {
+var wizards = [
+  {
+    name: WIZARD_NAMES[0],
+    coatColor: 'rgb(241, 43, 107)'
+  },
+  {
+    name: WIZARD_NAMES[1],
+    coatColor: 'rgb(215, 210, 55)'
+  },
+  {
+    name: WIZARD_NAMES[2],
+    coatColor: 'rgb(101, 137, 164)'
+  },
+  {
+    name: WIZARD_NAMES[3],
+    coatColor: 'rgb(127, 127, 127)'
+  }
+];
+
+for (var i = 0; i < wizards.length; i++) {
 
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = WIZARD_NAMES[i];
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
  
   similarListElement.appendChild(wizardElement);
  
- }
+}
