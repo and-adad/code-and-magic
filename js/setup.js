fac8c7331d31c12cf5d39c391b44d3375a320135
Андрей <Andreyadad@gmail.com>
2022-12-20 21:04:45 +0500

    После того, как код решает поставленную задачу, его можно улучшить - добавить ему структурируемости и читаемости

--- js/setup.js
+++ js/setup.js
@@ -4,9 +4,8 @@
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
-document.querySelector('.setup-similar').classList.remove('hidden');
+var similarListElement = userDialog.querySelector('.setup-similar-list');
 
-var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 var WIZARD_NAMES = ['Дамблдор', 'Волдемарт', 'Доктор Стрэндж', 'Гарри Поттер'];
@@ -30,13 +29,19 @@ var wizards = [
   }
 ];
 
-for (var i = 0; i < wizards.length; i++) {
-
+var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
- 
-  similarListElement.appendChild(wizardElement);
- 
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  
+  return wizardElement;
 }
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
