6b78ff55f4fd1217209423b8224e80707e16c0dc
Андрей <Andreyadad@gmail.com>
2023-01-07 19:44:55 +0500

    Аналогично добавил генерацию цветов плащей и глаз через функцию от входных данных

--- js/setup.js
+++ js/setup.js
@@ -8,10 +8,8 @@ document.querySelector('.setup-similar').classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-var DATA_COAT_COLOR = ['rgb(101, 137, 164', 'rgb(241, 43, 107', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var DATA_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-
 var generateName = function () {
+
     var DATA_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
     var DATA_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
     var name = DATA_NAMES[Math.floor(Math.random() * DATA_NAMES.length)];
@@ -20,6 +18,20 @@ var generateName = function () {
     return nameWizard;
 };
 
+var generateCoatColor = Function () {
+
+    var DATA_COAT_COLOR = ['rgb(101, 137, 164', 'rgb(241, 43, 107', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+    var coatColorWizard = DATA_COAT_COLOR[Math.floor(Math.random() * DATA_COAT_COLOR.length)];
+    return coatColorWizard;
+};
+
+var generateEyesColor = function () {
+
+    var DATA_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+    var eyesColorWizard = DATA_EYES_COLOR[Math.floor(Math.random() * DATA_EYES_COLOR.length)];
+    return eyesColorWizard;
+};
 
 
   for (var i = 0; i < 4; i++) {
