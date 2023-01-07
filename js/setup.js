21cabf713229ad2971634ccefd4fd7d3f2c2c424
Андрей <Andreyadad@gmail.com>
2023-01-07 21:51:08 +0500

    Удалил закомментированный код

--- js/setup.js
+++ js/setup.js
@@ -1,14 +1,5 @@
 'use strict';
 
-/*var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-
-document.querySelector('.setup-similar').classList.remove('hidden');
-
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-*/
-
 var generateWizardsJS = function (amount) {
   var dataWizards = [];
 
@@ -89,11 +80,3 @@ var addWizard = function (elementsDOM) {
 var wizardsJS = generateWizardsJS(4);
 var wizardsDOM = generateWizardsDOM(wizardsJS);
 addWizard(wizardsDOM);
-
-
-/*  for (var i = 0; i < 4; i++) {
-    var wizardElement = similarWizardTemplate.cloneNode(true);
-
-    similarListElement.appendChild(wizardElement);
-        
-};*/
