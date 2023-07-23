dab31189dfcdd114b62a182b1d859f1128949a7a
Андрей <Andreyadad@gmail.com>
2023-07-23 23:34:39 +0500

    Добавил обработчик на закрытие этого окна по клику на крестик

--- js/game.js
+++ js/game.js
@@ -2,7 +2,12 @@
 
 var setupOpen = document.querySelector('.setup-open');
 var setup = document.querySelector('.setup');
+var setupClose = setup.querySelector('.setup-close');
 
 setupOpen.addEventListener('click', function () {
   setup.classList.remove('hidden');
+});
+
+setupClose.addEventListener('click', function () {
+  setup.classList.add('hidden');
 });
