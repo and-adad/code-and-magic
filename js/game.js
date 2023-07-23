176349e4f67abb4fba69d3dee80ed62550d7eba1
Андрей <Andreyadad@gmail.com>
2023-07-23 23:15:17 +0500

    добавили всплывающее окно по нажатию на иконку

--- js/game.js
+++ js/game.js
@@ -1,3 +1,8 @@
 'use strict';
 
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
 
+setupOpen.addEventListener('click', function () {
+  setup.classList.remove('hidden');
+});
