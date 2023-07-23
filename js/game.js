f48af2ff69e6dd112016cc2ef2e16f05ad54ec8c
Андрей <Andreyadad@gmail.com>
2023-07-24 01:12:10 +0500

    Сделал ограничения редактирования имени пользователя

--- js/game.js
+++ js/game.js
@@ -11,3 +11,25 @@ setupOpen.addEventListener('click', function () {
 setupClose.addEventListener('click', function () {
   setup.classList.add('hidden');
 });
+
+var userNameInput = setup.querySelector('.setup-user-name');
+userNameInput.addEventListener('invalid', function (evt) {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+/*userNameInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length < 3) {
+    target.setCustomValidity('Имя должно состоять минимум из 3-х символов');
+  } else {
+    target.setCustomValidity('');
+  }
+});*/
